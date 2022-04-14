49553a8f00a2a106f025f703593fdb7dbbd2d42e
Jose M Perez <jmperezvzla@gmail.com>
2022-04-13 20:29:11 -0400

    Add Check Placement

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -2,6 +2,33 @@ const { ValidateStatus } = require('./status')
 
 class SudokuSolver {
 
+  // _getRowColumn(index) {
+  //   const row = Math.floor(index / 9)
+  //   const column = index % 9
+    
+  //   return { row, column }
+  // }
+
+  _getIndex(row, column) {
+    return (row * 9) + column
+  }
+
+  _createMatrix(puzzleString) {
+    const matrix = []
+
+    for(let i = 0; i < 9; i++) {
+      const vector = []
+      matrix.push(vector)
+      
+      for(let j = 0; j < 9; j++) {
+        const index = this._getIndex(i, j)
+        vector.push(puzzleString[index])
+      }
+    }
+
+    return matrix
+  }
+
   validate(puzzleString) {
     
     if (puzzleString.length !== 81) {
@@ -21,18 +48,52 @@ class SudokuSolver {
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    const matrix = this._createMatrix(puzzleString)
 
+    for(let j = 0; j < 9; j++) {
+      if (j !== column && matrix[row][j] === String(value)) {
+        return false
+      }
+    }
+    
+    return true
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    const matrix = this._createMatrix(puzzleString)
 
+    for(let i = 0; i < 9; i++) {
+      if (i !== row && matrix[i][column] === String(value)) {
+        return false
+      }
+    }
+    
+    return true
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    const matrix = this._createMatrix(puzzleString)
 
+    const base = 3
+    const rowRegion = Math.floor(row / base) * base
+    const colRegion = Math.floor(column / base) * base
+
+    for(let i = rowRegion; i < (rowRegion + base); i++) {
+      for(let j = colRegion; j < (colRegion + base); j++) {
+        if (i === row && j === column) continue
+
+        if (matrix[i][j] === String(value)) {
+          return false
+        }
+      }
+    }
+    
+    return true
   }
 
   solve(puzzleString) {
+    const matrix = this._createMatrix(puzzleString)
+
     
   }
 }
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,37 +1,98 @@
-const chai = require('chai');
+const mocha = require("mocha");
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver  = require('../controllers/sudoku-solver.js');
-const { ValidateStatus } = require('../controllers/status.js');
+const Solver = require("../controllers/sudoku-solver.js");
+const { ValidateStatus } = require("../controllers/status.js");
 
 let solver;
 
-suite('UnitTests', () => {
+suite("UnitTests", () => {
+  suite("Validate Puzzle", () => {
+    test("Logic handles a valid puzzle string of 81 characters", () => {
+      let puzzle =
+        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+      solver = new Solver();
+      const val = solver.validate(puzzle);
+      assert.equal(
+        val,
+        ValidateStatus.VALID,
+        "validate return should be VALID"
+      );
+    });
 
-    suite('Validate Puzzle', () => {
-        test('Logic handles a valid puzzle string of 81 characters', () => {
-            let puzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'  
-            solver = new Solver()
-            const val = solver.validate(puzzle)
-            assert.equal(val, ValidateStatus.VALID, 'validate return should be VALID')
-        })
+    test("Logic handles a puzzle string with invalid characters (not 1-9 or .)", () => {
+      let puzzle =
+        "1.5..2.84..63.12.7.2..5.-0..9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37X";
+      solver = new Solver();
+      const val = solver.validate(puzzle);
+      assert.equal(
+        val,
+        ValidateStatus.INVALIDCHARS,
+        "validate return should be INVALIDCHARS"
+      );
+    });
 
-        test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', () => {
-            let puzzle = '1.5..2.84..63.12.7.2..5.-0..9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37X'
-            solver = new Solver()
-            const val = solver.validate(puzzle)
-            assert.equal(val, ValidateStatus.INVALIDCHARS, 'validate return should be INVALIDCHARS')
-        })
+    test("Logic handles a puzzle string that is not 81 characters in length", () => {
+      let puzzle =
+        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.3";
+      solver = new Solver();
+      const val = solver.validate(puzzle);
+      assert.equal(
+        val,
+        ValidateStatus.INVALID81,
+        "validate return should be INVALID81"
+      );
+    });
+  });
 
-        test('Logic handles a puzzle string that is not 81 characters in length', () => {
-            let puzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.3'
-            solver = new Solver()
-            const val = solver.validate(puzzle)
-            assert.equal(val, ValidateStatus.INVALID81, 'validate return should be INVALID81')
-        })
-    })
+  suite("Check Placement", () => {
+    let puzzle =
+      "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+    solver = new Solver();
 
-    suite('Check Placement', () => {
+    test("Logic handles a valid row placement", () => {
+      const check = solver.checkRowPlacement(puzzle, 0, 1, 6);
+      assert.isTrue(check, "check return should be true");
+    });
+
+    test("Logic handles an invalid row placement", () => {
+      const check = solver.checkRowPlacement(puzzle, 0, 1, 2);
+      assert.isFalse(check, "check return should be false");
+    });
+
+    test("Logic handles a valid column placement", () => {
+      const check = solver.checkColPlacement(puzzle, 1, 1, 1);
+      assert.isTrue(check, "check return should be true");
+    });
+
+    test("Logic handles an invalid column placement", () => {
+      const check = solver.checkColPlacement(puzzle, 4, 1, 6);
+      assert.isFalse(check, "check return should be false");
+    });
+
+    test("Logic handles a valid region (3x3 grid) placement", () => {
+      const check = solver.checkRegionPlacement(puzzle, 5, 4, 5);
+      assert.isTrue(check, "check return should be true");
+    });
+
+    test("Logic handles an invalid region (3x3 grid) placement", () => {
+      const check = solver.checkRegionPlacement(puzzle, 3, 8, 7);
+      assert.isFalse(check, "check return should be false");
+    });
+  });
+
+  suite('Solver Puzzle', () => {
+
+    test('Valid puzzle strings pass the solver', () => {
+        let puzzle = "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+        solver = new Solver();
+
         
-    })
+    });
+
+    test('Invalid puzzle strings fail the solver', () => {
+
+    });
+  })
 });
