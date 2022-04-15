613f3a55a156cf7e724b0e65bea3975902907a43
Jose M Perez <jmperezvzla@gmail.com>
2022-04-14 21:19:01 -0400

    Create api & Functional Test /api/check

--- routes/api.js
+++ routes/api.js
@@ -1,41 +1,93 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
-const { ValidateStatus, SolverStatus } = require('../controllers/status')
+const SudokuSolver = require("../controllers/sudoku-solver.js");
+const { ValidateStatus, SolverStatus } = require("../controllers/status");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    const { puzzle, coordinate, value } = req.body;
 
-    });
+    if (typeof(puzzle) === 'undefined'
+      || typeof(coordinate) === 'undefined'
+      || typeof(value) === 'undefined') {
+      return res.json({ error: 'Required field(s) missing' });
+    }
+
+    const val = solver.validate(puzzle)
+
+    if (ValidateStatus.INVALIDCHARS === val) {
+      return res.json({ error: 'Invalid characters in puzzle' });
+    }
+
+    if (ValidateStatus.INVALID81 === val) {
+      return res.json({ error: 'Expected puzzle to be 81 characters long' });
+    }
+
+    const conflict = [];
+
+    const codeA = 'A'.charCodeAt(0);
+    const codeI = 'I'.charCodeAt(0);
+    const codeCoordRow = coordinate.charCodeAt(0);
+    const row = codeCoordRow - codeA;
+    const col = Number(coordinate[1]) - 1
+
+    if (codeCoordRow < codeA 
+      || codeCoordRow > codeI
+      || col < 0
+      || col > 8) {
+      return res.json({ error: 'Invalid coordinate' })
+    }
     
-  app.route('/api/solve')
-    .post((req, res) => {
+    const valueNum = Number(value)
 
-      const { puzzle } = req.body;
-      
-      if (typeof(puzzle) === 'undefined') {
-        return res.json({ error: 'Required field missing' })
-      }
+    if (isNaN(valueNum)
+     || valueNum < 1
+     || valueNum > 9) {
+      return res.json({ error: 'Invalid value' })
+    }
 
-      const validate = solver.validate(puzzle)
-      if (ValidateStatus.INVALIDCHARS === validate) {
-        return res.json({ error: 'Invalid characters in puzzle' })
-      }
-      if (ValidateStatus.INVALID81 === validate) {
-        return res.json({ error: 'Expected puzzle to be 81 characters long' })
-      }
+    let ok = solver.checkRowPlacement(puzzle, row, col, value);
+    if (!ok) conflict.push("row");
+    ok = solver.checkColPlacement(puzzle, row, col, value);
+    if (!ok) conflict.push("column");
+    ok = solver.checkRegionPlacement(puzzle, row, col, value);
+    if (!ok) conflict.push("region");
 
-      const sol = solver.solve(puzzle);
+    if (conflict.length > 0) {
+      return res.json({
+        valid: false,
+        conflict,
+      });
+    }
 
-      if (SolverStatus.INVALID === sol.status) {
-        return res.json({ error: 'Puzzle cannot be solved' })
-      }
-
-      res.json(sol);
-
+    res.json({
+      valid: true,
     });
+  });
+
+  app.route("/api/solve").post((req, res) => {
+    const { puzzle } = req.body;
+
+    if (typeof puzzle === "undefined") {
+      return res.json({ error: "Required field missing" });
+    }
+
+    const validate = solver.validate(puzzle);
+    if (ValidateStatus.INVALIDCHARS === validate) {
+      return res.json({ error: "Invalid characters in puzzle" });
+    }
+    if (ValidateStatus.INVALID81 === validate) {
+      return res.json({ error: "Expected puzzle to be 81 characters long" });
+    }
+
+    const sol = solver.solve(puzzle);
+
+    if (SolverStatus.INVALID === sol.status) {
+      return res.json({ error: "Puzzle cannot be solved" });
+    }
+
+    res.json(sol);
+  });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,40 +1,328 @@
 const chai = require("chai");
-const chaiHttp = require('chai-http');
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server');
-const { puzzlesAndSolutions } = require('../controllers/puzzle-strings')
+const server = require("../server");
+const { puzzlesAndSolutions } = require("../controllers/puzzle-strings");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', () => {
+suite("Functional Tests", () => {
+  suite("Solve", () => {
+    test("Solve a puzzle with valid puzzle string: POST request to /api/solve", (done) => {
+      const puzzle1 = puzzlesAndSolutions[0][0];
+      const solPuzzle1 = puzzlesAndSolutions[0][1];
+      const data = {
+        puzzle: puzzle1,
+      };
 
-    test('Solve a puzzle with valid puzzle string: POST request to /api/solve', (done) => {
-        const puzzle1 = puzzlesAndSolutions[0][0]
-        const solPuzzle1 = puzzlesAndSolutions[0][1]
-        const data = {
-            puzzle: puzzle1
-        }
-
-        chai
+      chai
         .request(server)
         .post("/api/solve")
         .send(data)
         .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(
+            res.body,
+            "solution",
+            "response should contain solution"
+          );
+          assert.equal(
+            res.body.solution,
+            solPuzzle1,
+            `solution should be "${solPuzzle1}"`
+          );
+          done();
+        });
+    });
+
+    test("Solve a puzzle with missing puzzle string: POST request to /api/solve", (done) => {
+      const data = {};
+      const error = "Required field missing";
+
+      chai
+        .request(server)
+        .post("/api/solve")
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(res.body, "error", "response should contain error");
+          assert.equal(res.body.error, error, `solution should be "${error}"`);
+          done();
+        });
+    });
+
+    test("Solve a puzzle with invalid characters: POST request to /api/solve", (done) => {
+      const data = {
+        puzzle:
+          "1.5..2.84..63X12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+      };
+      const error = "Invalid characters in puzzle";
+
+      chai
+        .request(server)
+        .post("/api/solve")
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(res.body, "error", "response should contain error");
+          assert.equal(res.body.error, error, `solution should be "${error}"`);
+          done();
+        });
+    });
+
+    test("Solve a puzzle with incorrect length: POST request to /api/solve", (done) => {
+      const data = {
+        puzzle:
+          "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.",
+      };
+      const error = "Expected puzzle to be 81 characters long";
+
+      chai
+        .request(server)
+        .post("/api/solve")
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(res.body, "error", "response should contain error");
+          assert.equal(res.body.error, error, `solution should be "${error}"`);
+          done();
+        });
+    });
+
+    test("Solve a puzzle that cannot be solved: POST request to /api/solve", (done) => {
+      const data = {
+        puzzle:
+          "115..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+      };
+      const error = "Puzzle cannot be solved";
+
+      chai
+        .request(server)
+        .post("/api/solve")
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(res.body, "error", "response should contain error");
+          assert.equal(res.body.error, error, `solution should be "${error}"`);
+          done();
+        });
+    });
+  });
+
+  suite("Check", () => {
+    test("Check a puzzle placement with all fields: POST request to /api/check", (done) => {
+      const data = {
+        puzzle: puzzlesAndSolutions[0][0],
+        coordinate: "B2",
+        value: 8,
+      };
+
+      chai
+        .request(server)
+        .post("/api/check")
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(res.body, "valid", "response should contain valid");
+          assert.isTrue(res.body.valid, `valid should be "True"`);
+          done();
+        });
+    });
+
+    test("Check a puzzle placement with single placement conflict: POST request to /api/check", (done) => {
+      const data = {
+        puzzle: puzzlesAndSolutions[0][0],
+        coordinate: "E4",
+        value: 4,
+      };
+
+      chai
+        .request(server)
+        .post("/api/check")
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(res.body, "valid", "response should contain valid");
+          assert.property(
+            res.body,
+            "conflict",
+            "response should contain conflict"
+          );
+
+          assert.isFalse(res.body.valid, `valid should be "False"`);
+          assert.isArray(res.body.conflict, "conflict should be a Array");
+          assert.include(
+            res.body.conflict.join(" "),
+            "row",
+            'conflict should have "row"'
+          );
+          done();
+        });
+    });
+
+    test("Check a puzzle placement with multiple placement conflict: POST request to /api/check", (done) => {
+      const data = {
+        puzzle: puzzlesAndSolutions[0][0],
+        coordinate: "B2",
+        value: 7,
+      };
+
+      chai
+        .request(server)
+        .post("/api/check")
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(res.body, "valid", "response should contain valid");
+          assert.property(
+            res.body,
+            "conflict",
+            "response should contain conflict"
+          );
+
+          assert.isFalse(res.body.valid, `valid should be "False"`);
+          assert.isArray(res.body.conflict, "conflict should be a Array");
+          const conflict = res.body.conflict.join(" ");
+          assert.include(conflict, "row", 'conflict should have "row"');
+          assert.include(conflict, "column", 'conflict should have "column"');
+          done();
+        });
+    });
+
+    test("Check a puzzle placement with all placement conflict: POST request to /api/check", (done) => {
+      const data = {
+        puzzle: puzzlesAndSolutions[0][0],
+        coordinate: "I8",
+        value: 9,
+      };
+
+      chai
+        .request(server)
+        .post("/api/check")
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(res.body, "valid", "response should contain valid");
+          assert.property(
+            res.body,
+            "conflict",
+            "response should contain conflict"
+          );
+
+          assert.isFalse(res.body.valid, `valid should be "False"`);
+          assert.isArray(res.body.conflict, "conflict should be a Array");
+          const conflict = res.body.conflict.join(" ");
+          assert.include(conflict, "row", 'conflict should have "row"');
+          assert.include(conflict, "column", 'conflict should have "column"');
+          assert.include(conflict, "region", 'conflict should have "region"');
+          done();
+        });
+    });
+
+    test("Check a puzzle placement with missing required fields: POST request to /api/check", (done) => {
+        const data = {
+          puzzle: puzzlesAndSolutions[0][0]
+        };
+  
+        const error = "Required field(s) missing";
+    
+        chai
+        .request(server)
+        .post("/api/check")
+        .send(data)
+        .end(function (err, res) {
             assert.equal(res.status, 200);
             assert.isObject(res.body, "response should be an object");
-            assert.property(res.body, "solution", "response should contain solution");
-            assert.equal(res.body.solution, solPuzzle1, `solution should be "${solPuzzle1}"`);
+            assert.property(res.body, "error", "response should contain error");
+            assert.equal(res.body.error, error, `solution should be "${error}"`);
             done();
         });
     });
 
-    test('Solve a puzzle with missing puzzle string: POST request to /api/solve', (done) => {
-        const data = {}
-        const error = 'Required field missing'
-
+    test("Check a puzzle placement with invalid characters: POST request to /api/check", (done) => {
+        const data = {
+            puzzle: '1.5..2.84..63.12.7.2..5.....9..1..P.8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
+            coordinate: 'B2',
+            value: 4
+        };
+        const error = "Invalid characters in puzzle";
+    
         chai
         .request(server)
-        .post("/api/solve")
+        .post("/api/check")
+        .send(data)
+        .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.isObject(res.body, "response should be an object");
+            assert.property(res.body, "error", "response should contain error");
+            assert.equal(res.body.error, error, `solution should be "${error}"`);
+            done();
+        });
+    });
+
+    test("Check a puzzle placement with incorrect length: POST request to /api/check", (done) => {
+        const data = {
+            puzzle: '1...5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
+            coordinate: 'B2',
+            value: 4
+        };
+        const error = "Expected puzzle to be 81 characters long";
+    
+        chai
+        .request(server)
+        .post("/api/check")
+        .send(data)
+        .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.isObject(res.body, "response should be an object");
+            assert.property(res.body, "error", "response should contain error");
+            assert.equal(res.body.error, error, `solution should be "${error}"`);
+            done();
+        });
+    });
+    
+    test("Check a puzzle placement with invalid placement coordinate: POST request to /api/check", (done) => {
+        const data = {
+            puzzle: '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
+            coordinate: 'X0',
+            value: 4
+        };
+        const error = "Invalid coordinate";
+    
+        chai
+        .request(server)
+        .post("/api/check")
+        .send(data)
+        .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.isObject(res.body, "response should be an object");
+            assert.property(res.body, "error", "response should contain error");
+            assert.equal(res.body.error, error, `solution should be "${error}"`);
+            done();
+        });
+    });
+
+    test("Check a puzzle placement with invalid placement value: POST request to /api/check", done => {
+
+        const data = {
+            puzzle: '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
+            coordinate: 'C2',
+            value: 'X'
+        };
+        const error = "Invalid value";
+    
+        chai
+        .request(server)
+        .post("/api/check")
         .send(data)
         .end(function (err, res) {
             assert.equal(res.status, 200);
@@ -46,65 +334,5 @@ suite('Functional Tests', () => {
 
     });
 
-    test('Solve a puzzle with invalid characters: POST request to /api/solve', (done) => {
-        const data = {
-            puzzle: '1.5..2.84..63X12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
-        }
-        const error = 'Invalid characters in puzzle'
-
-        chai
-        .request(server)
-        .post("/api/solve")
-        .send(data)
-        .end(function (err, res) {
-            assert.equal(res.status, 200);
-            assert.isObject(res.body, "response should be an object");
-            assert.property(res.body, "error", "response should contain error");
-            assert.equal(res.body.error, error, `solution should be "${error}"`);
-            done();
-        });
-
-    });
-
-    test('Solve a puzzle with incorrect length: POST request to /api/solve', (done) => {
-        const data = {
-            puzzle: '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.'
-        }
-        const error = 'Expected puzzle to be 81 characters long'
-
-        chai
-        .request(server)
-        .post("/api/solve")
-        .send(data)
-        .end(function (err, res) {
-            assert.equal(res.status, 200);
-            assert.isObject(res.body, "response should be an object");
-            assert.property(res.body, "error", "response should contain error");
-            assert.equal(res.body.error, error, `solution should be "${error}"`);
-            done();
-        });
-
-    });
-
-    test('Solve a puzzle that cannot be solved: POST request to /api/solve', (done) => {
-        const data = {
-            puzzle: '115..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
-        }
-        const error = 'Puzzle cannot be solved'
-
-        chai
-        .request(server)
-        .post("/api/solve")
-        .send(data)
-        .end(function (err, res) {
-            assert.equal(res.status, 200);
-            assert.isObject(res.body, "response should be an object");
-            assert.property(res.body, "error", "response should contain error");
-            assert.equal(res.body.error, error, `solution should be "${error}"`);
-            done();
-        });
-
-    });
-
+  });
 });
-
